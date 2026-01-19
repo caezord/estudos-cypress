c5d5e170d3a2a0e129f207e01180dbde47f560ef
pestesexual <caemarques71@gmail.com>
2026-01-19 17:30:34 -0300

    add faker lib + criação de dados de user para teste

--- e2e/primeiro-teste/cypress/e2e/teste-ecommerce.cy.js
+++ e2e/primeiro-teste/cypress/e2e/teste-ecommerce.cy.js
@@ -1,4 +1,8 @@
-describe('tela de login', () => {     // decribe -> "tela sendo testada"
+import { faker } from '@faker-js/faker'
+
+
+describe('tela de login', () => {                                 // decribe -> "tela sendo testada"
+
     it('criar conta com email já cadastrado', () => {            // it -> caso de teste
         cy.fixture('example').then((example) => {
             cy.visit('https://www.automationexercise.com/login')
@@ -10,15 +14,23 @@ describe('tela de login', () => {     // decribe -> "tela sendo testada"
         })
 
     })
-
+ 
     it('criar conta com novo email', () => {
+        const user_testing = {
+            nome: faker.person.fullName(),
+          }
         cy.fixture('usuario').then((usuario) => {
             cy.visit('https://www.automationexercise.com/login')
             cy.get('[data-qa="signup-name"]').type(usuario.name)
             cy.get('[data-qa="signup-email"]').type(usuario.email)
             cy.get('[data-qa="signup-button"]').click()
 
-
+            // verificar se está na página correta
+            cy.get(':nth-child(1) > b').should('have.text', 'Enter Account Information')
+            
+            cy.get(':nth-child(3) > .top > [data-qa="title"] > span > [name="title"]').check()
+            cy.get('[data-qa="first_name"]').type(user_testing.nome)
         })
     })
 })
+
